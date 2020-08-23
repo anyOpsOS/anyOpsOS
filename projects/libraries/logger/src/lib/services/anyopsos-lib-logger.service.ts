fa60a4993cdafe8fbe7e036ccdef8ddbe2d13f5c
Isart Navarro <contact@isartnavarro.io>
2020-08-23 19:32:02 +0000

    fix(lib-logger): prevent circular stringify

--- projects/libraries/logger/src/lib/services/anyopsos-lib-logger.service.ts
+++ projects/libraries/logger/src/lib/services/anyopsos-lib-logger.service.ts
@@ -1,48 +1,53 @@
 import {Injectable} from '@angular/core';
 
-import {NGXLogger} from 'ngx-logger';
+import {NGXLogger, CustomNGXLoggerService, NgxLoggerLevel} from 'ngx-logger';
 
+import {AnyOpsOSLibUtilsService} from '@anyopsos/lib-utils';
 import {MatSnackBar} from '@anyopsos/lib-angular-material';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AnyOpsOSLibLoggerService {
+export class AnyOpsOSLibLoggerService{
+  private logger: NGXLogger;
 
-  constructor(private readonly logger: NGXLogger,
-              private readonly snackBar: MatSnackBar) {
+  constructor(private readonly customLogger: CustomNGXLoggerService,
+              private readonly snackBar: MatSnackBar,
+              private readonly LibUtils: AnyOpsOSLibUtilsService) {
+
+    this.logger = this.customLogger.create({ level: NgxLoggerLevel.TRACE });
 
   }
 
-  trace(entry: string, message: string, args?: IArguments, ...anything: any[]): void {
-    this.logger.trace(`[${entry}] -> ${message} -> ${JSON.stringify(args)} -> ${anything}`);
+  trace(entry: string, message: string, args?: IArguments | string[], ...anything: any[]): void {
+    this.logger.trace(`[${entry}] -> ${message} -> ${this.LibUtils.stringify(args)} -> ${anything}`);
   }
 
-  debug(entry: string, message: string, args?: IArguments, ...anything: any[]): void {
-    this.logger.debug(`[${entry}] -> ${message} -> ${JSON.stringify(args)} -> ${anything}`);
+  debug(entry: string, message: string, args?: IArguments | string[], ...anything: any[]): void {
+    this.logger.debug(`[${entry}] -> ${message} -> ${this.LibUtils.stringify(args)} -> ${anything}`);
   }
 
-  info(entry: string, message: string, args?: IArguments, ...anything: any[]): void {
-    this.logger.info(`[${entry}] -> ${message} -> ${JSON.stringify(args)} -> ${anything}`);
+  info(entry: string, message: string, args?: IArguments | string[], ...anything: any[]): void {
+    this.logger.info(`[${entry}] -> ${message} -> ${this.LibUtils.stringify(args)} -> ${anything}`);
   }
 
-  log(entry: string, message: string, args?: IArguments, ...anything: any[]): void {
-    this.logger.log(`[${entry}] -> ${message} -> ${JSON.stringify(args)} -> ${anything}`);
+  log(entry: string, message: string, args?: IArguments | string[], ...anything: any[]): void {
+    this.logger.log(`[${entry}] -> ${message} -> ${this.LibUtils.stringify(args)} -> ${anything}`);
     this.snackBar.open(message, entry, { panelClass: 'bg-success' });
   }
 
-  warn(entry: string, message: string, args?: IArguments, ...anything: any[]): void {
-    this.logger.warn(`[${entry}] -> ${message} -> ${JSON.stringify(args)} -> ${anything}`);
+  warn(entry: string, message: string, args?: IArguments | string[], ...anything: any[]): void {
+    this.logger.warn(`[${entry}] -> ${message} -> ${this.LibUtils.stringify(args)} -> ${anything}`);
     this.snackBar.open(message, entry, { panelClass: 'bg-warning' });
   }
 
-  error(entry: string, message: string, args?: IArguments, ...anything: any[]): void {
-    this.logger.error(`[${entry}] -> ${message} -> ${JSON.stringify(args)} -> ${anything}`);
+  error(entry: string, message: string, args?: IArguments | string[], ...anything: any[]): void {
+    this.logger.error(`[${entry}] -> ${message} -> ${this.LibUtils.stringify(args)} -> ${anything}`);
     this.snackBar.open(message, entry, { panelClass: 'bg-danger' });
   }
 
-  fatal(entry: string, message: string, args?: IArguments, ...anything: any[]): void {
-    this.logger.fatal(`[${entry}] -> ${message} -> ${JSON.stringify(args)} -> ${anything}`);
+  fatal(entry: string, message: string, args?: IArguments | string[], ...anything: any[]): void {
+    this.logger.fatal(`[${entry}] -> ${message} -> ${this.LibUtils.stringify(args)} -> ${anything}`);
     this.snackBar.open(message, entry, { panelClass: 'bg-danger' });
   }
 }
